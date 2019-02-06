0402d0fd3f717043203600af54f2910205f1849c
Måns Anderssone <mans.andersson97@gmail.com>
2019-02-06 18:05:45 +0100

    Initial commit. Currently rendering a static image. Some game elements and logic implemented.

---
+++ Game.hs
@@ -0,0 +1,36 @@
+module Game where
+
+data Direction = Left | Right | Up | Down deriving Eq
+data PBlock = PBlock {getDir :: Direction, xCoord :: Integer, yCoord :: Integer}
+type GameState = (Player, Player)
+type Player = [PBlock]
+
+rows = 50
+columns = 100
+
+initialGameState = ([PBlock Up 20 20, PBlock Up 20 21], [PBlock Up 70 20, PBlock Up 70 21])
+
+getDirectionChar :: Direction -> Char
+getDirectionChar Game.Left = '←'
+getDirectionChar Game.Right = '→'
+getDirectionChar Game.Up = '↑'
+getDirectionChar Game.Down = '↓'
+
+movePlayer :: Player -> Player
+movePlayer p@(h:s:_) = newBlock:p
+  where dir = getDir h
+        newBlock = nextBlock h s
+
+movePlayers :: GameState -> GameState
+movePlayers (p1,p2) = (movePlayer p1, movePlayer p2)
+
+nextBlock :: PBlock -> PBlock -> PBlock
+nextBlock (PBlock d1 x1 y1) (PBlock d2 x2 y2)
+  | (d1, d2) == (Game.Left, Game.Right) = PBlock d1 (x1+1) y1
+  | (d1, d2) == (Game.Right, Game.Left) = PBlock d1 (x1-1) y1
+  | (d1, d2) == (Game.Up, Game.Down) = PBlock d1 x1 (y1+1)
+  | (d1, d2) == (Game.Down, Game.Up) = PBlock d1 x1 (y1-1)
+  | d1 == Game.Left = PBlock d1 (x1-1) y1
+  | d1 == Game.Right = PBlock d1 (x1+1) y1
+  | d1 == Game.Up = PBlock d1 x1 (y1-1)
+  | d1 == Game.Down = PBlock d1 x1 (y1+1)
---
+++ main.hs
@@ -0,0 +1,37 @@
+import UI.NCurses
+import Data.Time.Clock.System
+import Control.Monad.IO.Class(liftIO)
+import Game
+
+main = runCurses $ do
+  w <- newWindow (rows+1) (columns+1) 0 0
+  gameLoop w initialGameState
+
+gameLoop :: Window -> GameState -> Curses ()
+gameLoop w gs@(p1,p2) = do
+  updateWindow w $ do
+    sequence drawWalls
+    sequence (drawPlayer p1)
+    sequence (drawPlayer p2)
+  render
+  t <- liftIO getSystemTime
+  gameLoop w gs
+
+drawChar :: Char -> Integer -> Integer -> Update ()
+drawChar ch row col = do
+  moveCursor row col
+  drawString [ch]
+
+drawPBlock :: Char -> PBlock -> Update ()
+drawPBlock ch pb = drawChar ch (yCoord pb) (xCoord pb)
+
+drawWalls :: [Update ()]
+drawWalls = leftright ++ topbottom
+  where leftright = (drawChar '#') <$> [0..rows-1] <*> [0,columns-1]
+        topbottom = (drawChar '#') <$> [0,rows-1] <*> [0..columns-1]
+
+drawPlayer :: Player -> [Update ()]
+drawPlayer [] = []
+drawPlayer (h:t) = (drawPBlock dirChar h):(drawPlayerTail t)
+  where drawPlayerTail t = map (drawPBlock '#') t
+        dirChar = getDirectionChar(getDir h)
