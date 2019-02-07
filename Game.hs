301cc027f5cb278df2bd475d9584006bce933a11
Måns Anderssone <mans.andersson97@gmail.com>
2019-02-07 20:45:53 +0100

    Player inputs now added. Game is controllable but no collisions are currently being checked.

--- Game.hs
+++ Game.hs
@@ -6,16 +6,21 @@ type Timestamp = Integer
 type GameState = (Player, Player, Timestamp)
 type Player = [PBlock]
 
-rows = 50
-columns = 100
+rows = 60
+columns = 60
 
-initialGameState = ([PBlock Up 20 20, PBlock Up 20 21], [PBlock Up 70 20, PBlock Up 70 21], 0)
+initialGameState = ([PBlock Up 20 20, PBlock Up 20 21], [PBlock Up 30 20, PBlock Up 30 21], 0)
 
 getDirectionChar :: Direction -> Char
 getDirectionChar Game.Left = '←'
 getDirectionChar Game.Right = '→'
 getDirectionChar Game.Up = '↑'
 getDirectionChar Game.Down = '↓'
+
+changeDirection :: Player -> Direction -> Player
+changeDirection (h:t) d = (PBlock d currentX currentY):t
+  where currentX = xCoord h
+        currentY = yCoord h
 
 movePlayer :: Player -> Player
 movePlayer p@(h:s:_) = newBlock:p
@@ -29,15 +34,15 @@ movePlayers (p1,p2,ts) = (movePlayer p1, movePlayer p2,ts)
 This controls the pace of the game.-}
 moveGame :: GameState -> Integer-> GameState
 moveGame gs@(p1,p2,ts) time
-  | (time - ts) > 1000 = movePlayers (p1,p2,time)
+  | (time - ts) > 400 = movePlayers (p1,p2,time)
   | otherwise = gs
 
 nextBlock :: PBlock -> PBlock -> PBlock
 nextBlock (PBlock d1 x1 y1) (PBlock d2 x2 y2)
-  | (d1, d2) == (Game.Left, Game.Right) = PBlock d1 (x1+1) y1
-  | (d1, d2) == (Game.Right, Game.Left) = PBlock d1 (x1-1) y1
-  | (d1, d2) == (Game.Up, Game.Down) = PBlock d1 x1 (y1+1)
-  | (d1, d2) == (Game.Down, Game.Up) = PBlock d1 x1 (y1-1)
+  | (d1, d2) == (Game.Left, Game.Right) = PBlock d2 (x1+1) y1
+  | (d1, d2) == (Game.Right, Game.Left) = PBlock d2 (x1-1) y1
+  | (d1, d2) == (Game.Up, Game.Down) = PBlock d2 x1 (y1+1)
+  | (d1, d2) == (Game.Down, Game.Up) = PBlock d2 x1 (y1-1)
   | d1 == Game.Left = PBlock d1 (x1-1) y1
   | d1 == Game.Right = PBlock d1 (x1+1) y1
   | d1 == Game.Up = PBlock d1 x1 (y1-1)
--- main.hs
+++ main.hs
@@ -1,9 +1,11 @@
 import UI.NCurses
 import Data.Time.Clock.POSIX
 import Control.Monad.IO.Class(liftIO)
+import Data.Char
 import Game
 
 main = runCurses $ do
+  setEcho False
   w <- newWindow (rows+1) (columns+1) 0 0
   gameLoop w initialGameState
 
@@ -16,7 +18,7 @@ gameLoop w gs@(p1,p2,ts) = do
   render
   t <- liftIO getPOSIXTime
   e <- getEvent w (Just 1)
-  gameLoop w (moveGame gs (getMilliSeconds t))
+  gameLoop w $ readEvent e ((moveGame gs (getMilliSeconds t)))
 
 drawChar :: Char -> Integer -> Integer -> Update ()
 drawChar ch row col = do
@@ -39,3 +41,16 @@ drawPlayer (h:t) = (drawPBlock dirChar h):(drawPlayerTail t)
 
 getMilliSeconds :: POSIXTime -> Integer
 getMilliSeconds t = round $ t * 1000
+
+readEvent :: Maybe Event -> GameState -> GameState
+readEvent (Just (EventCharacter k)) gs@(p1,p2,ts)
+  | k `isKey` 'w' = ((changeDirection p1 Game.Up),p2,ts)
+  | k `isKey` 'a' = ((changeDirection p1 Game.Left),p2,ts)
+  | k `isKey` 's' = ((changeDirection p1 Game.Down),p2,ts)
+  | k `isKey` 'd' = ((changeDirection p1 Game.Right),p2,ts)
+  | k `isKey` 'i' = (p1,(changeDirection p2 Game.Up),ts)
+  | k `isKey` 'j' = (p1,(changeDirection p2 Game.Left),ts)
+  | k `isKey` 'k' = (p1,(changeDirection p2 Game.Down),ts)
+  | k `isKey` 'l' = (p1,(changeDirection p2 Game.Right),ts)
+    where isKey k i = k == i || k == (toUpper i)
+readEvent _ gs = gs
